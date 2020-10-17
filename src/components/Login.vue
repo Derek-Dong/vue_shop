41dac0e4e69b228b370d11650ad1b18f9870a25e
Derek-Dong <Derek-Dong@foxmail.com>
2020-10-17 23:01:57 +0800

    完成了登陆前表单预验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,20 @@
         <img src="../assets/logo.png" alt="">
       </div>
       <!-- 登录表单区域 -->
-      <el-form label-width="0px" class="login_form">
+      <el-form ref="loginFromRef" :model="loginForm" label-width="0px" class="login_form" :rules="loginFormRules">
         <!-- 用户名 -->
-        <el-form-item>
-          <el-input prefix-icon="iconfont icon-user"></el-input>
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item>
-          <el-input prefix-icon="iconfont icon-3702mima"></el-input>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima"
+          type="password"></el-input>
         </el-form-item>
         <!-- 按钮区域 -->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -27,8 +28,45 @@
 
 <script>
 export default {
+  data(){
+    return {
+      // 登录表单的数据绑定对象
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 表单的验证规则对象
+      loginFormRules:{
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: "请输入登录名称", trigger: "blur" },
+          { min: 3, max: 10, message: "长度在3到10个字符", trigger: "blur" }
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: "请输入登录密码", trigger: "blur" },
+          { min: 6, max: 15, message: "长度在6到15个字符", trigger: "blur" }
+        ],
+      },
+    };
+  },
 
-}
+  methods: {
+  // 点击重置按钮，重置登录表单
+    resetLoginForm() {
+      this.$refs.loginFromRef.resetFields();
+    },
+    login(){
+      this.$refs.loginFromRef.validate(async valid => {
+        // console.log(valid)
+        if (!valid) return;
+        const { date: res } = await this.$http.post("login", this.loginForm);
+        if (res.meta.status !==200) return this.$message.error("登陆失败！");
+        this.$message.success("登陆成功");
+      });
+    }
+  },
+};
 </script>
 
 <style lang="less" scoped>
