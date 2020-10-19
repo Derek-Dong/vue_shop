919e2dc3e8f9e2078e70e057f09de74ee447702b
Derek-Dong <Derek-Dong@foxmail.com>
2020-10-19 17:22:25 +0800

    完成了主页和用户列表

--- src/main.js
+++ src/main.js
@@ -10,6 +10,14 @@ import './assets/css/global.css'
 import axios from 'axios'
 // 配置根路径
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+    // axios请求拦截
+axios.interceptors.request.use(config => {
+    // 为请求头对象，添加Token验证Authorization字段
+    console.log(config)
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+        // 在最后必须return config
+    return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,13 +1,50 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-import { Form, FormItem } from 'element-ui'
-import { Input } from 'element-ui'
-// 导入弹框提示组件
-import { Message } from 'element-ui'
+// Message导入弹框提示组件
+import {
+    Button,
+    Form,
+    FormItem,
+    Input,
+    Message,
+    Container,
+    Header,
+    Aside,
+    Main,
+    Menu,
+    Submenu,
+    MenuItem,
+    Breadcrumb,
+    BreadcrumbItem,
+    Card,
+    Row,
+    Col,
+    Table,
+    TableColumn,
+    Switch,
+    Tooltip,
+    Pagination,
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
     // $message自定义属性，可以任意改名
 Vue.prototype.$message = Message
