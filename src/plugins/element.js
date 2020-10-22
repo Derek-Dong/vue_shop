f33463436810dc86b47b4639fcae6a3b4d50ffe3
Derek-Dong <Derek-Dong@foxmail.com>
2020-10-22 23:12:33 +0800

    完成了权限功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -25,6 +25,10 @@ import {
     Pagination,
     Dialog,
     MessageBox,
+    Tag,
+    Tree,
+    Select,
+    Option
 } from 'element-ui'
 
 Vue.use(Button)
@@ -49,6 +53,10 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
     // $message自定义属性，可以任意改名
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
