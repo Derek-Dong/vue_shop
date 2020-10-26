259f051b2e1056c685646220b35b4ad18b25ed50
Derek-Dong <Derek-Dong@foxmail.com>
2020-10-26 18:08:35 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -9,6 +9,13 @@ import './assets/css/global.css'
 // 导入vue-table-with-tree-grid树形表格组件
 import TreeTable from 'vue-table-with-tree-grid'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require styles导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 import axios from 'axios'
 // 配置根路径
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
@@ -25,6 +32,24 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+    // 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+Vue.filter('dateFormat', function(originVal) {
+    const dt = new Date(originVal)
+
+    const y = dt.getFullYear()
+        // padStart() 方法用另一个字符串填充当前字符串(如果需要的话，会重复多次)
+        // 以便产生的字符串达到给定的长度。从当前字符串的左侧开始填充。
+    const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+    const d = (dt.getDate() + '').padStart(2, '0')
+
+    const hh = (dt.getHours() + '').padStart(2, '0')
+    const mm = (dt.getMinutes() + '').padStart(2, '0')
+    const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+    return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
     router,
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -33,6 +33,11 @@ import {
     Alert,
     Tabs,
     TabPane,
+    Steps,
+    Step,
+    CheckboxGroup,
+    Checkbox,
+    Upload,
 } from 'element-ui'
 
 Vue.use(Button)
@@ -65,6 +70,11 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
     // $message自定义属性，可以任意改名
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
