f5ae19d9c07d42a4859f0b2171566d9d5b79952f
Derek-Dong <Derek-Dong@foxmail.com>
2021-01-04 21:10:27 +0800

    更改element样式的导入

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue_shop/'
+    : '/',
     chainWebpack: config => {
         // 发布模式
         config.when(process.env.NODE_ENV === 'production', config => {
